0b214cd88c259f469cdddb18f767567184347059
iburuyane <iburuyane@gmail.com>
2020-01-29 21:49:57 +0200

    Moved go_to_login_page and should_be_login_link from MainPage to BasePage. Added test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page to the test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
+from .pages.login_page import LoginPage
 import time
 import pytest
 
@@ -16,6 +17,7 @@ def test_guest_can_add_product_to_basket(browser, offer_number):
     page.should_be_price_in_basket_and_in_message()
     print(link)
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page=ProductPage(browser, link)
@@ -29,10 +31,26 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page=ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.should_dissapear_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser,link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
